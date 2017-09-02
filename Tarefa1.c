38124df9203f575a1f24e7025830cfd746771913
Júnior <Babilonia364@users.noreply.github.com>
2017-09-02 16:59:17 -0300

    Update Tarefa1.c
    Questão 2 adicionada

--- Tarefa1.c
+++ Tarefa1.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
-double Soma();
+void Soma();									//Calcular a media de 5 valores usando apenas 2 variaveis
+void Ordena();									//Ordenar quatro palavras de 3 letras
 
-double Soma()									//Funcao de media da questão 1
+void Soma()										//Funcao de media da questão 1
 {
 	double valor;
 	double quantidade=0;
@@ -22,7 +24,130 @@ double Soma()									//Funcao de media da questão 1
 	printf("%f\n", valor);
 }
 
+void Ordena()									//Funcao para ordenar storingues
+{
+	char pala1[4]="abc", pala2[4]="bcd", pala3[4]="cde", pala4[4]="efg";
+	int val1=0, val2=0,val3=0;
+	//fgets(pala1, 3, stdin);											//To tendo problema com fgets, alguem se habilita a resolver?
+	//fgets(pala2, 3, stdin);
+	//fgets(pala3, 3, stdin);
+	//fgets(pala4, 3, stdin);
+	val1=strcmp(pala1, pala2);
+	val2=strcmp(pala2, pala3);
+	val3=strcmp(pala3, pala4);
+	if(val1<0 && val2<0 && val3<0)										//Gambiarra logica
+	{
+		printf("Ordem: %s\n%s\n%s\n%s\n", pala1, pala2, pala3, pala4);
+	}else if(val1>0 && val2<0 && val3<0)
+	{
+		val1=strcmp(pala1, pala4);
+		if(val1>0)
+		{
+			printf("Ordem: %s\n%s\n%s\n%s\n", pala2, pala3, pala4, pala1);
+		} else
+		{
+			val1=strcmp(pala1,pala3);
+			if(val1>0)
+			{
+				printf("Ordem: %s\n%s\n%s\n%s\n", pala2, pala3, pala1, pala4);
+			} else
+			{
+				printf("Ordem: %s\n%s\n%s\n%s\n", pala2, pala1, pala3, pala4);
+			}
+		}
+	}else if(val1<0 && val2>0 && val3<0)
+	{
+		val1=strcmp(pala1, pala4);
+		if(val1>0)
+		{
+			printf("Ordem: %s\n%s\n%s\n%s\n", pala3, pala4, pala1, pala2);
+		} else
+		{
+			val1=strcmp(pala2, pala4);
+			if(val1>0)
+			{
+				printf("Ordem: %s\n%s\n%s\n%s\n", pala3, pala1, pala4, pala2);
+			} else {printf("Ordem: %s\n%s\n%s\n%s\n", pala3, pala1, pala2, pala4);}
+		}
+	}else if(val1<0 && val2<0 && val3>0)
+	{
+		val1=strcmp(pala2, pala4);
+		if(val1<0)
+		{
+			printf("Ordem: %s\n%s\n%s\n%s\n", pala1, pala2, pala4, pala3);
+		} else
+		{
+			val1=strcmp(pala1, pala4);
+			if(val1>0)
+			{
+				printf("Ordem: %s\n%s\n%s\n%s\n", pala4, pala1, pala2, pala3);
+			} else{printf("Ordem: %s\n%s\n%s\n%s\n", pala1, pala4, pala2, pala3);}
+		}
+	}else if(val1>0 && val2>0 && val3<0)
+	{
+		val1=strcmp(pala2, pala4);
+		if(val1>0)
+		{
+			printf("Ordem: %s\n%s\n%s\n%s\n", pala3, pala4, pala2, pala1);
+		}else
+		{
+			val1=strcmp(pala1, pala4);
+			if(val1<0)
+			{
+				printf("Ordem: %s\n%s\n%s\n%s\n", pala3, pala2, pala1, pala4);
+			} else{printf("Ordem: %s\n%s\n%s\n%s\n", pala3, pala2, pala4, pala1);}
+		}
+	}else if(val1>0 && val2<0 && val3>0)
+	{
+		val1=strcmp(pala1, pala3);
+		val2=strcmp(pala2, pala4);
+		if(val1>0 && val2>0)
+		{
+			printf("Ordem: %s\n%s\n%s\n%s\n", pala4, pala2, pala3, pala1);
+		}else if(val1>0 && val2<0)
+		{
+			printf("Ordem: %s\n%s\n%s\n%s\n", pala2, pala4, pala3, pala1);
+		}else if(val1<0 && val2>0)
+		{
+			printf("Ordem: %s\n%s\n%s\n%s\n", pala4, pala2, pala1, pala3);
+		}else{printf("Ordem: %s\n%s\n%s\n%s\n", pala2, pala4, pala1, pala3);}
+	}else if(val1<0 && val2>0 && val3>0)
+	{
+		val1=strcmp(pala1, pala3);
+		if(val1>0)
+		{
+			printf("Ordem: %s\n%s\n%s\n%s\n", pala4, pala3, pala1, pala2);
+		}else
+		{
+			val1=strcmp(pala1,pala4);
+			if(val1>0)
+			{
+				printf("Ordem: %s\n%s\n%s\n%s\n", pala4, pala1, pala3, pala2);
+			}else{printf("Ordem: %s\n%s\n%s\n%s\n", pala1, pala4, pala3, pala2);}
+		}
+	}else if(val1>0 && val2>0 && val3>0)
+	{
+		printf("Ordem: %s\n%s\n%s\n%s\n", pala4, pala3, pala2, pala1);
+	}
+}
+
 int main()
 {
-	Soma();
+	/*int menu;
+	printf("1 - Media\n2- Ordenacao\n")
+	scanf("%d", menu);
+	switch()
+	{
+		case 1:
+			Soma();
+		break;
+		
+		case 2:
+			Ordena();
+		break;
+		
+		default:
+		return 0;
+	}*/
+	Ordena();
 }
