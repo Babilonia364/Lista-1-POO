4161483bd1ad069934ef468a3c0a52ea39b7faa9
Júnior <Babilonia364@users.noreply.github.com>
2017-09-04 22:15:33 -0300

    Update Tarefa2.c

--- Tarefa2.c
+++ Tarefa2.c
@@ -1,6 +1,9 @@
 #include <stdio.h>									//To viciado 'Heidis Küken - Das kleine Küken piept', ouçam, e mto bom!!!
+#include <string.h>
 
 int ler (int V[], int n, int A);					//Funcao recursiva que identifica o elemento maximo do vetor
+void replace();										//Recebe duas strings e substitui todas as ocorrencias da segunda string na primeira
+
 
 int ler (int V[], int n, int A)
 {
@@ -20,8 +23,31 @@ int ler (int V[], int n, int A)
 	}else{return A;}								//Se chegou, abortar missao
 }
 
+void replace()										//Essa deu trabalho, nossa sem hora
+{
+	char str1[]="abcdabcdbbacdbba", str2[]="cd", *pont;				//Caso teste
+	char temp[strlen(str1)];
+	
+	while((pont=strstr(str1, str2))!=NULL)							//Funcao da biblioteca padrao string, olhar documentacao
+	{
+		strncat(temp, str1, (strlen(str1)-strlen(pont)));			//Funcao de adicao de string da biblioteca padrao
+		strcat(temp, "*");											//Funcao de adicao de "apendice" da biblio padrao string.h
+		strcpy(str1, (pont+(strlen(str2)*sizeof(char))));			//Funcao de copia de string da biblio string.h
+	}																//A funcao acima copia apagando o que a string anterior continha
+	strncat(temp, str1, (strlen(str1)));							//Se ainda houver pedaço faltando da string 1, sera adicionado aqui
+	printf("temp e:%s\n", temp);
+}
+
 int main()
 {
-	int V[3], A=10, n=3;
-	ler(V, n, A);
+/*	int menu=0;
+	scanf("%d", &menu);
+	switch(menu)
+	{
+		case 1:
+		{
+			break;
+		}
+	}
+	*/
 }
