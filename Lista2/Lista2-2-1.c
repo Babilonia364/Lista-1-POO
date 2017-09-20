34f613317424da5d8c9318833f70016fb97e4747
Júnior <Babilonia364@users.noreply.github.com>
2017-09-20 18:34:58 -0300

    Add files via upload
    Adicionando primeira questão da lista 4

---
+++ Lista2/Lista2-2-1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>														//Questao 1
+
+struct Pessoa
+{
+	int rg;
+	int cpf;
+	char nome[80];
+};
+struct Pessoa cadastro[100];
+
+int buscaBinaria(struct Pessoa cadastro[], int tamanho, int buscado)	//Para entender sobre busca binaria, ver: https://www.ime.usp.br/~pf/algoritmos/aulas/bubi2.html
+{
+	int esquerda, direita, meio;
+	esquerda=0;															//MAS O QUE E ISSO?
+	direita=tamanho;
+	while(esquerda<=direita)
+	{
+		meio=(esquerda+direita)/2;
+		if(cadastro[meio].rg==buscado)
+			return meio;
+		if(cadastro[meio].rg<buscado)
+			esquerda=meio+1;
+		else
+			direita=meio-1;
+	}
+	return -1;
+	
+}
+
+int main()
+{
+	/*					//Bem, o algoritmo funciona, boa sorte tentando ordenar 100 rgs diferentes para testar
+	int buscado, j;
+	for(j=0; j<100; j++)
+		scanf("%d", &cadastro[j].rg);
+	printf("Digite o rg que deseja encontrar\n");
+	scanf("%d", &buscado);
+	j=buscaBinaria(cadastro, 100, buscado);
+	if(j!=-1)
+		printf("O indice do rg %d nos arquivos eh %d\n", buscado, j);
+	else
+		printf("rg invalido\n");
+	*/
+}
