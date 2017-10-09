98d0c100d7358043fa49df749d68212f178af8c9
Júnior <Babilonia364@users.noreply.github.com>
2017-10-09 00:14:17 -0300

    Update Lista2-3-1.c

--- Lista3/Lista2-3-1.c
+++ Lista3/Lista2-3-1.c
@@ -8,50 +8,50 @@ typedef struct employee
 	double salary;
 }employee;
 
-int add(char fname[], long empID, char stringName[], double salary)					//Funcao para adicionar funcionarios para o quadro da empresa
+int add(char fname[], long empID, char stringName[], double salary)			//Funcao para adicionar funcionarios para o quadro da empresa
 {
-	FILE *arq;																		//Ponteiro de arquivo
-	employee aux;																	//auxiliar do struct employee
-	arq=fopen(fname, "r+b");														//Abre arquivo binario (.bin) para leitura e escrita
+	FILE *arq;									//Ponteiro de arquivo
+	employee aux;									//auxiliar do struct employee
+	arq=fopen(fname, "r+b");							//Abre arquivo binario (.bin) para leitura e escrita
 	if(arq==NULL)
 		printf("Erro. Arquivo nao existe\n");
-	while(fread(&aux, sizeof(employee), 1, arq)!=0)									//Verifica todo o arquivo em busca da posição final
+	while(fread(&aux, sizeof(employee), 1, arq)!=0)					//Verifica todo o arquivo em busca da posição final
 	{
-		if(aux.id==empID)															//Se houver id repetido, o programa fecha
+		if(aux.id==empID)							//Se houver id repetido, o programa fecha
 		{
 			printf("Funcionario ja se encontra cadastrado");
 			return 0;
 		}
 	}
-	strcpy(aux.name, stringName);													//Substitui o nome da string do struct auxiliar pelo nome do funcionario
-	aux.id=empID;																	//Analogo para o id
-	aux.salary=salary;																//Analogo para o salario
-	fwrite(&aux, sizeof(employee), 1, arq);											//Escreve as informacoes do funcionario no arquivo
-	fclose(arq);																	//Fecha o arquivo, boas praticas de programacao minaaaaahhh
+	strcpy(aux.name, stringName);							//Substitui o nome da string do struct auxiliar pelo nome do funcionario
+	aux.id=empID;									//Analogo para o id
+	aux.salary=salary;								//Analogo para o salario
+	fwrite(&aux, sizeof(employee), 1, arq);						//Escreve as informacoes do funcionario no arquivo
+	fclose(arq);									//Fecha o arquivo, boas praticas de programacao minaaaaahhh
 	return 0;
 }
 
-void moreVapiroes(char fname[], long empID, double incr)							//Funcao para adicionar aumento salarial
+void moreVapiroes(char fname[], long empID, double incr)				//Funcao para adicionar aumento salarial
 {
 	FILE *arq;
 	employee aux;
-	int cachacinha=0;																//Flag que vai apontar se o funcionario existe
+	int cachacinha=0;								//Flag que vai apontar se o funcionario existe
 	arq=fopen(fname, "r+b");
 	if(arq==NULL)
 		printf("\nErro, documento nao criado\n");
-	while(fread(&aux, sizeof(employee), 1, arq)!=0)									//Loop para ler o arquivo ate o fim
+	while(fread(&aux, sizeof(employee), 1, arq)!=0)					//Loop para ler o arquivo ate o fim
 	{
-		if(aux.id==empID)															//Se o funcionario existir, a flag e setada para 1
+		if(aux.id==empID)							//Se o funcionario existir, a flag e setada para 1
 		{
 			cachacinha=1;
 			break;
 		}
 	}
-	if(cachacinha)																	//Em C, 1 e V, qualquer outro numero e F, logo, como a flag subiu e setou 1, o if e V
+	if(cachacinha)									//Em C, 1 e V, qualquer outro numero e F, logo, como a flag subiu e setou 1, o if e V
 	{
-		aux.salary=aux.salary+incr;													//Incrementa o salario
-		fseek(arq, -1*sizeof(employee), SEEK_CUR);									//Retorna uma posicao do arquivo, para que ele sobrescreva o funcionario existente e nao adicione mais um
-		fwrite(&aux, sizeof(employee), 1, arq);										//Sobrescreve o funcionario
+		aux.salary=aux.salary+incr;						//Incrementa o salario
+		fseek(arq, -1*sizeof(employee), SEEK_CUR);				//Retorna uma posicao do arquivo, para que ele sobrescreva o funcionario existente e nao adicione mais um
+		fwrite(&aux, sizeof(employee), 1, arq);					//Sobrescreve o funcionario
 		printf("\nAumento efetuado com sucesso.\nO novo salario e: %.2lf\n", aux.salary);
 	}
 	else
@@ -73,7 +73,7 @@ void show(char fname[])
 int main()
 {
 	int menu;
-	char nomeArq[100];																//Nome do arquivo.bin
+	char nomeArq[100];								//Nome do arquivo.bin
 	printf("Digite o nome do arquivo do quadro de funcionarios da empresa\n\n");
 	scanf("%s", nomeArq);
 	getchar();
